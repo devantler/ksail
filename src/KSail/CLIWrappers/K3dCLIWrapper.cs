4e8243a359a5b11b2ac9c414ba545b38bac3819a
devantler <nikolaiemildamm@icloud.com>
2024-01-12 02:38:25 +0100

    Add GetClusterAsync method to K3dCLIWrapper and use it in KSailUpCommandHandler

--- src/KSail/CLIWrappers/K3dCLIWrapper.cs
+++ src/KSail/CLIWrappers/K3dCLIWrapper.cs
@@ -1,5 +1,6 @@
 using System.Runtime.InteropServices;
 using CliWrap;
+using CliWrap.Buffered;
 
 namespace KSail.CLIWrappers;
 
@@ -47,6 +48,13 @@ static class K3dCLIWrapper
   {
     var cmd = K3d.WithArguments($"cluster delete {name}");
     _ = await CLIRunner.RunAsync(cmd);
+  }
+
+  internal static async Task<bool> GetClusterAsync(string name)
+  {
+    var cmd = K3d.WithArguments($"cluster get {name}").WithValidation(CommandResultValidation.None);
+    var result = await cmd.ExecuteBufferedAsync();
+    return result.ExitCode == 0;
   }
 
   internal static async Task ListClustersAsync()
