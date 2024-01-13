91d7c1297720733266625143cc274ba0b67b7c89
devantler <nikolaiemildamm@icloud.com>
2024-01-13 23:16:48 +0100

    Refactor SOPSProvisioner class and remove unused code

--- src/KSail/Provisioners/SOPSProvisioner.cs
+++ src/KSail/Provisioners/SOPSProvisioner.cs
@@ -1,116 +1,37 @@
-using System.Text.RegularExpressions;
 using KSail.CLIWrappers;
 
-namespace KSail.Provisioners.SecretManagement;
+namespace KSail.Provisioners;
 
-sealed partial class SOPSProvisioner : IProvisioner, IDisposable
+sealed class SOPSProvisioner : IProvisioner, IDisposable
 {
-  [GeneratedRegex("export KSAIL_SOPS_PUBLIC_GPG_KEY='.*'")]
-  private static partial Regex KSailSOPSPublicKeyFilter();
-
-  [GeneratedRegex("export KSAIL_SOPS_PRIVATE_GPG_KEY='.*'")]
-  private static partial Regex KSailSOPSPrivateKeyFilter();
-
   readonly KubernetesProvisioner kubernetesProvisioner = new();
 
   internal static async Task CreateKeysAsync()
   {
-    string? existingKey = Environment.GetEnvironmentVariable("KSAIL_SOPS_PRIVATE_GPG_KEY");
-    if (!string.IsNullOrWhiteSpace(existingKey))
+    if (File.Exists("ksail_sops.agekey"))
     {
-      Console.WriteLine("✔ Using existing SOPS GPG key from environment variables...");
+      Console.WriteLine("✔ Using existing SOPS key...");
       return;
     }
 
-    Console.WriteLine("► Generating new SOPS GPG key and saving it to environment variables...");
-    await GPGCLIWrapper.CreateGPGKeyAsync();
-
-    string envFilePath = GetEnvironmentFilePath();
-    string envFileContent = File.ReadAllText(envFilePath);
-
-    string privateKey = await GPGCLIWrapper.ExportPrivateKeyAsync(true);
-    string publicKey = await GPGCLIWrapper.ExportPublicKeyAsync(true);
-    const string figPostBlock = "# Fig post block. Keep at the bottom of this file.";
-    string privateKeyExportStatement = $"export KSAIL_SOPS_PRIVATE_GPG_KEY='{privateKey}'";
-    string publicKeyExportStatement = $"export KSAIL_SOPS_PUBLIC_GPG_KEY='{publicKey}'";
-
-    envFileContent = ReplaceOrInsertExportStatement(envFileContent, privateKeyExportStatement, KSailSOPSPrivateKeyFilter(), figPostBlock);
-    envFileContent = ReplaceOrInsertExportStatement(envFileContent, publicKeyExportStatement, KSailSOPSPublicKeyFilter(), figPostBlock);
-
-    File.WriteAllText(envFilePath, envFileContent);
-    Environment.SetEnvironmentVariable("KSAIL_SOPS_PRIVATE_GPG_KEY", privateKey);
-    Environment.SetEnvironmentVariable("KSAIL_SOPS_PUBLIC_GPG_KEY", publicKey);
+    Console.WriteLine("► Generating new SOPS key...");
+    await AgeKeygenCLIWrapper.GenerateKeyAsync();
   }
 
   public async Task ProvisionAsync()
   {
+    string ageKey = await File.ReadAllTextAsync("ksail_sops.agekey");
     await kubernetesProvisioner.CreateSecretAsync("sops-gpg", new Dictionary<string, string>
     {
-      ["sops.asc"] = Environment.GetEnvironmentVariable("KSAIL_SOPS_PRIVATE_GPG_KEY") ??
-        throw new InvalidOperationException("🚨 Could not find the SOPS GPG key in the KSAIl_SOPS_PRIVATE_GPG_KEY environment variable.")
+      ["sops.asc"] = ageKey
     }, "flux-system");
   }
 
-  internal static async Task CreateSOPSConfigAsync(string manifestsPath)
-  {
-    Console.WriteLine("► Creating SOPS config file...");
-    string sopsConfigPath = $"{manifestsPath}/../.sops.yaml";
-    string fingerprint = await GPGCLIWrapper.GetFingerprintAsync();
-    string sopsConfigContent = $"""
-      creation_rules:
-        - path_regex: .sops.yaml
-          encrypted_regex: ^(data|stringData)$
-          pgp: ${fingerprint}
-      """;
+  internal static Task CreateSOPSConfigAsync(string configPath) => throw new NotImplementedException();
 
-    await File.WriteAllTextAsync(sopsConfigPath, sopsConfigContent);
-  }
+  internal static Task ShowPublicKeyAsync() => throw new NotImplementedException();
 
-  internal static async Task ShowPublicKeyAsync()
-  {
-    string publicKey = Environment.GetEnvironmentVariable("KSAIL_SOPS_PUBLIC_GPG_KEY") ?? await GPGCLIWrapper.ExportPublicKeyAsync(true);
-    Console.WriteLine(publicKey);
-    Console.WriteLine();
-  }
-
-  internal static async Task ShowPrivateKeyAsync()
-  {
-    string privateKey = Environment.GetEnvironmentVariable("KSAIL_SOPS_PRIVATE_GPG_KEY") ?? await GPGCLIWrapper.ExportPrivateKeyAsync(true);
-    Console.WriteLine(privateKey);
-    Console.WriteLine();
-  }
-
-  static string GetEnvironmentFilePath()
-  {
-    string envFilePath = Environment.GetFolderPath(Environment.SpecialFolder.UserProfile);
-    if (File.Exists($"{envFilePath}/.zshrc"))
-    {
-      return $"{envFilePath}/.zshrc";
-    }
-    else if (File.Exists($"{envFilePath}/.bashrc"))
-    {
-      return $"{envFilePath}/.bashrc";
-    }
-
-    throw new FileNotFoundException("🚨 Could not save SOPS GPG key to environment variables because neither .zshrc nor .bashrc were found in the user's home directory.");
-  }
-
-  static string ReplaceOrInsertExportStatement(string envFileContent, string exportStatement, Regex filterRegex, string insertMarker)
-  {
-    if (envFileContent.Contains(exportStatement))
-    {
-      return filterRegex.Replace(envFileContent, exportStatement);
-    }
-    else if (envFileContent.Contains(insertMarker))
-    {
-      int insertIndex = envFileContent.IndexOf(insertMarker, StringComparison.InvariantCulture);
-      return envFileContent.Insert(insertIndex, $"{exportStatement}\n");
-    }
-    else
-    {
-      return $"{envFileContent}\n{exportStatement}";
-    }
-  }
+  internal static Task ShowPrivateKeyAsync() => throw new NotImplementedException();
 
   public void Dispose()
   {
