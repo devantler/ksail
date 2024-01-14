7d83212aaf37a4b2ffead33756a8b4e922666dca
devantler <nikolaiemildamm@icloud.com>
2024-01-14 03:45:42 +0100

    Remove unnecessary code for bundle extraction directory

--- src/KSail/Program.cs
+++ src/KSail/Program.cs
@@ -9,8 +9,6 @@ if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
 }
 else
 {
-  // Set bundle extraction directory to ~/.ksail/assets/binaries
-  Environment.SetEnvironmentVariable("DOTNET_BUNDLE_EXTRACT_BASE_DIR", $"{Environment.GetFolderPath(Environment.SpecialFolder.UserProfile)}/.ksail");
   Environment.SetEnvironmentVariable("SOPS_AGE_KEY_FILE", $"{Environment.GetFolderPath(Environment.SpecialFolder.UserProfile)}/.ksail/ksail_sops.agekey");
   foreach (string file in Directory.GetFiles($"{AppContext.BaseDirectory}assets/binaries"))
   {
