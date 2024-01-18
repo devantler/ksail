92da21c598f4e44e2e291e31e0698cf3840694b1
devantler <nikolaiemildamm@icloud.com>
2024-01-18 22:49:01 +0100

    Update order of commands

--- src/KSail/Commands/Root/KSailRootCommand.cs
+++ src/KSail/Commands/Root/KSailRootCommand.cs
@@ -31,13 +31,13 @@ sealed class KSailRootCommand : RootCommand
   {
     AddCommand(new KSailInitCommand());
     AddCommand(new KSailUpCommand());
+    AddCommand(new KSailListCommand());
+    AddCommand(new KSailUpdateCommand());
     AddCommand(new KSailStartCommand());
     AddCommand(new KSailStopCommand());
-    AddCommand(new KSailListCommand());
     AddCommand(new KSailDownCommand());
-    AddCommand(new KSailUpdateCommand());
-    AddCommand(new KSailLintCommand());
     AddCommand(new KSailCheckCommand());
+    AddCommand(new KSailLintCommand());
     AddCommand(new KSailSOPSCommand());
   }
 }
