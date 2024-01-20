bb91033aa6b86bec62754e8b8ae73903ccc85bab
Nikolai Emil Damm <nikolaiemildamm@icloud.com>
2024-01-20 04:32:54 +0100

    Remove print from K3dProvisioner ListAsync method (#124)

--- src/KSail/Provisioners/K3dProvisioner.cs
+++ src/KSail/Provisioners/K3dProvisioner.cs
@@ -17,11 +17,7 @@ sealed class K3dProvisioner() : IProvisioner
     await K3dCLIWrapper.DeleteClusterAsync(name);
   }
 
-  internal static async Task ListAsync()
-  {
-    Console.WriteLine("📋 Listing K3d clusters...");
-    _ = await K3dCLIWrapper.ListClustersAsync();
-  }
+  internal static async Task ListAsync() => _ = await K3dCLIWrapper.ListClustersAsync();
 
   internal static async Task<bool> ExistsAsync(string name) =>
     await K3dCLIWrapper.GetClusterAsync(name);
