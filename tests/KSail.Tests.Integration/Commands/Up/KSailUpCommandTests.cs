b457fbd275accb351ac4a89e2d00d8d68fdd0403
devantler <nikolaiemildamm@icloud.com>
2024-01-25 13:50:23 +0100

    Add test for ksail up with environment variables

--- tests/KSail.Tests.Integration/Commands/Up/KSailUpCommandTests.cs
+++ tests/KSail.Tests.Integration/Commands/Up/KSailUpCommandTests.cs
@@ -2,6 +2,7 @@ using System.CommandLine;
 using System.CommandLine.IO;
 using KSail.CLIWrappers;
 using KSail.Commands.Init;
+using KSail.Commands.SOPS;
 using KSail.Commands.Up;
 using KSail.Tests.Integration.TestUtils;
 
@@ -91,6 +92,37 @@ public class KSailUpCommandTests : IAsyncLifetime
     //Act
     int initExitCode = await ksailInitCommand.InvokeAsync("ksail", testConsole);
     int upExitCode = await ksailUpCommand.InvokeAsync("ksail", testConsole);
+    string clusters = await K3dCLIWrapper.ListClustersAsync();
+
+    //Assert
+    Assert.Equal(0, initExitCode);
+    Assert.Equal(0, upExitCode);
+    Assert.True(await CheckRegistriesExistAsync());
+    _ = await Verify(clusters);
+  }
+
+  /// <summary>
+  /// Tests that the <c>ksail up [name]</c> command with environment variables succeeds and creates a cluster.
+  /// </summary>
+  [Fact]
+  public async void KSailUpNameAndConfigAndEnv_SucceedsAndCreatesCluster()
+  {
+    Console.WriteLine($"🧪 Running {nameof(KSailUpNameAndConfig_SucceedsAndCreatesCluster)} test...");
+    //Arrange
+    var console = new TestConsole();
+    var ksailInitCommand = new KSailInitCommand();
+    var ksailUpCommand = new KSailUpCommand();
+    var ksailSOPSCommand = new KSailSOPSCommand();
+
+    //Act
+    if (!File.Exists(Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.UserProfile), ".ksail", "ksail_sops.agekey")))
+    {
+      _ = await ksailSOPSCommand.InvokeAsync("--generate-key", console);
+    }
+    string key = File.ReadAllText(Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.UserProfile), ".ksail", "ksail_sops.agekey"));
+    Environment.SetEnvironmentVariable("KSAIL_SOPS_KEY", key);
+    int initExitCode = await ksailInitCommand.InvokeAsync("ksail", console);
+    int upExitCode = await ksailUpCommand.InvokeAsync("ksail", console);
     string clusters = await K3dCLIWrapper.ListClustersAsync();
 
     //Assert
