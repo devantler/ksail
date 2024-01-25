cbe405943f36e6f5e18a38a0ab7bd1e45e9e4747
devantler <nikolaiemildamm@icloud.com>
2024-01-25 06:47:09 +0100

    Refactor KSailDownCommandTests class

--- tests/KSail.Tests.Integration/Commands/Down/KSailDownCommandTests.cs
+++ tests/KSail.Tests.Integration/Commands/Down/KSailDownCommandTests.cs
@@ -1,21 +1,14 @@
 using System.CommandLine;
 using System.CommandLine.IO;
 using KSail.Commands.Down;
-using KSail.Commands.Init;
-using KSail.Commands.Up;
-using KSail.Tests.Integration.TestUtils;
 
 namespace KSail.Tests.Integration.Commands.Down;
 
 /// <summary>
 /// Tests for the <see cref="KSailDownCommand"/> class.
 /// </summary>
-[Collection("KSail.Tests.Integration")]
-public class KSailDownCommandTests : IAsyncLifetime
+public class KSailDownCommandTests
 {
-  /// <inheritdoc/>
-  public async Task InitializeAsync() => await KSailTestUtils.Cleanup();
-
   /// <summary>
   /// Tests that the <c>ksail down</c> command fails and prints help.
   /// </summary>
@@ -34,42 +27,4 @@ public class KSailDownCommandTests : IAsyncLifetime
     Assert.Equal(1, exitCode);
     _ = await Verify(console.Error.ToString() + console.Out);
   }
-
-  /// <summary>
-  /// Tests that the <c>ksail down [name]</c> command succeeds and deletes a cluster.
-  /// </summary>
-  [Fact]
-  public async void KSailDownName_SucceedsAndDeletesCluster()
-  {
-    Console.WriteLine($"🧪 Running {nameof(KSailDownName_SucceedsAndDeletesCluster)} test...");
-    //Arrange
-    var console = new TestConsole();
-    var ksailInitCommand = new KSailInitCommand();
-    var ksailUpCommand = new KSailUpCommand();
-    var ksailDownCommand = new KSailDownCommand();
-
-    //Act
-    int initExitCode = await ksailInitCommand.InvokeAsync("ksail", console);
-    int upExitCode = await ksailUpCommand.InvokeAsync("ksail", console);
-    int downExitCode = await ksailDownCommand.InvokeAsync("ksail --delete-pull-through-registries", console);
-
-    //Assert
-    Assert.Equal(0, initExitCode);
-    Assert.Equal(0, upExitCode);
-    Assert.Equal(0, downExitCode);
-    Assert.False(await CheckRegistriesExistsAsync());
-  }
-
-  static async Task<bool> CheckRegistriesExistsAsync()
-  {
-    return await DockerTestUtils.ContainerExistsAsync("proxy-docker.io")
-      && await DockerTestUtils.ContainerExistsAsync("proxy-registry.k8s.io")
-      && await DockerTestUtils.ContainerExistsAsync("proxy-gcr.io")
-      && await DockerTestUtils.ContainerExistsAsync("proxy-ghcr.io")
-      && await DockerTestUtils.ContainerExistsAsync("proxy-quay.io")
-      && await DockerTestUtils.ContainerExistsAsync("proxy-mcr.microsoft.com");
-  }
-
-  /// <inheritdoc/>
-  public async Task DisposeAsync() => await KSailTestUtils.Cleanup();
 }
