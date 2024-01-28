7e20c85dbe995cad8ac7902dfadafb901f283693
devantler <nikolaiemildamm@icloud.com>
2024-01-29 00:26:24 +0100

    Add Flux installation and configuration

--- src/KSail/Provisioners/GitOps/FluxProvisioner.cs
+++ src/KSail/Provisioners/GitOps/FluxProvisioner.cs
@@ -9,11 +9,15 @@ sealed class FluxProvisioner : IGitOpsProvisioner
     Console.WriteLine("🔼 Checking Flux prerequisites are satisfied...");
     await FluxCLIWrapper.CheckPrerequisitesAsync(context);
     Console.WriteLine();
+
     Console.WriteLine("🔼 Installing Flux...");
     await FluxCLIWrapper.InstallAsync(context);
+    Console.WriteLine();
+
     Console.WriteLine("🔼 Creating Flux OCI source...");
     await FluxCLIWrapper.CreateSourceOCIAsync(context, sourceUrl);
     Console.WriteLine();
+
     Console.WriteLine("🔼 Creating Flux kustomization...");
     await FluxCLIWrapper.CreateKustomizationAsync(context, path);
     Console.WriteLine();
