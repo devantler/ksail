30c6f488cc76dbc66e7ad7bba5ed3ccbec0b6d5f
devantler <nikolaiemildamm@icloud.com>
2024-02-18 15:53:15 +0100

    Refactor binary handling in get_wrapped_binaries.sh

--- scripts/get_wrapped_binaries.sh
+++ scripts/get_wrapped_binaries.sh
@@ -32,7 +32,7 @@ download_and_update() {
           echo "Moving binary from subfolder $subfolder"
           mv -f src/KSail/assets/binaries/"${subfolder}"/"$binary" src/KSail/assets/binaries/"${binary}"_"${arch}"
           rm -rf src/KSail/assets/binaries/"${subfolder}"
-        elif [ ! -e src/KSail/assets/binaries/"${binary}"_"${arch}" ]; then
+        elif [ -e src/KSail/assets/binaries/"$binary" ]; then
           mv -f src/KSail/assets/binaries/"$binary" src/KSail/assets/binaries/"${binary}"_"${arch}"
         fi
         chmod +x src/KSail/assets/binaries/"${binary}"_"${arch}"
