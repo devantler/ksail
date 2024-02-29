d80be80b495e0d641fff6634de79e7921c20020f
Nikolai Emil Damm <ned@devantler.com>
2024-02-29 03:02:03 +0100

    Update Kustomization.cs

--- src/KSail/Models/Kubernetes/Kustomization.cs
+++ src/KSail/Models/Kubernetes/Kustomization.cs
@@ -2,7 +2,7 @@ using KSail.Generators.Models;
 
 namespace KSail.Models.Kubernetes;
 
-class Kustomization : IModel
+class Kustomization
 {
   public string Namespace { get; set; } = "";
   public required List<string> Resources { get; set; } = [];
