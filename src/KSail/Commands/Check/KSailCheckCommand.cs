c7c772151cb3c7604808a00bef526c773c4f1d03
Nikolai Emil Damm <nikolaiemildamm@icloud.com>
2024-03-14 01:02:09 +0100

    Remove timeout option from KSailUpCommand and improve healthchecking to fail early (#227)
    * Remove timeout option from KSailUpCommand and improve healthchecking to fail early.
    
    * Add elapsed time to console output in KSailCheckCommandHandler
    
    * Refactor console output for elapsed time in KSailCheckCommandHandler.cs
    
    * Refactor console output formatting in KSailCheckCommandHandler
    
    * Refactor console output in KSailCheckCommandHandler
    
    * Add null check for kustomization status in KSailCheckCommandHandler.cs
    
    * Fix typo in variable name
    
    * Refactor KSailCheckCommandHandler class
    
    * Update console output in KSailCheckCommandHandler.cs
    
    * Refactor KSailCheckCommandHandler class
    
    * Update console output message in KSailCheckCommandHandler.cs
    
    * Remove console log statement in KSailCheckCommandHandler
    
    * Refactor KSailCheckCommandHandler.cs

--- src/KSail/Commands/Check/KSailCheckCommand.cs
+++ src/KSail/Commands/Check/KSailCheckCommand.cs
@@ -2,7 +2,6 @@ using System.CommandLine;
 using KSail.Arguments;
 using KSail.Commands.Check.Handlers;
 using KSail.Commands.Check.Options;
-using KSail.Options;
 using KSail.Provisioners.KubernetesDistribution;
 
 namespace KSail.Commands.Check;
@@ -11,13 +10,11 @@ sealed class KSailCheckCommand : Command
 {
   readonly ClusterNameArgument _clusterNameArgument = new() { Arity = ArgumentArity.ExactlyOne };
   readonly KubeconfigOption _kubeconfigOption = new() { IsRequired = true };
-  readonly TimeoutOption _timeoutOption = new();
 
   internal KSailCheckCommand() : base("check", "Check the status of the cluster")
   {
     AddArgument(_clusterNameArgument);
     AddOption(_kubeconfigOption);
-    AddOption(_timeoutOption);
     AddValidator(result =>
     {
       string? kubeconfig = result.GetValueForOption(_kubeconfigOption);
@@ -31,7 +28,6 @@ sealed class KSailCheckCommand : Command
       string clusterName = context.ParseResult.GetValueForArgument(_clusterNameArgument);
       string kubeconfig = context.ParseResult.GetValueForOption(_kubeconfigOption) ??
         throw new InvalidOperationException("Kubeconfig not set");
-      int timeout = context.ParseResult.GetValueForOption(_timeoutOption);
 
       var kubernetesDistributionProvisioner = new K3dProvisioner();
       var kubernetesDistributionType = await kubernetesDistributionProvisioner.GetKubernetesDistributionTypeAsync();
@@ -41,7 +37,7 @@ sealed class KSailCheckCommand : Command
       var handler = new KSailCheckCommandHandler();
       try
       {
-        context.ExitCode = await handler.HandleAsync(k8sContext, timeout, token, kubeconfig);
+        context.ExitCode = await handler.HandleAsync(k8sContext, token, kubeconfig);
       }
       catch (OperationCanceledException)
       {
