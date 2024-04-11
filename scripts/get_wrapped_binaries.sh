6b5c53d729cc9fc1d80a1a2e0ed101ab5b31bd36
devantler <nikolaiemildamm@icloud.com>
2024-04-11 23:51:31 +0200

    Update get_wrapped_binaries.sh script to download binaries for additional architectures

--- scripts/get_wrapped_binaries.sh
+++ scripts/get_wrapped_binaries.sh
@@ -4,52 +4,31 @@ download_and_update() {
   binary=$2
   is_tarball=$3
   subfolder=$4
-  architectures=("darwin.amd64" "darwin.arm64" "linux.amd64" "linux.arm64")
-
-  echo "Fetching latest release information for $repo"
+  architectures=("darwin-amd64" "darwin_amd64" "darwin-arm64" "darwin_arm64" "linux-amd64" "linux_amd64" "linux-arm64" "linux_arm64")
   latest_release=$(curl -s https://api.github.com/repos/"$repo"/releases/latest)
   version_latest=$(echo "$latest_release" | grep tag_name | cut -d '"' -f 4 | cut -d '/' -f 2)
-  version_current=$(grep -s "${binary}" src/KSail/assets/binaries/requirements.txt | cut -d '_' -f 2)
-  if [ -z "$version_current" ]; then
-    version_current="v0.0.0"
-  fi
-  echo "Latest version: $version_latest"
-  echo "Current version: $version_current"
 
-  if [ "$version_latest" != "$version_current" ]; then
-    echo "Updating $binary to version $version_latest"
-    for arch in "${architectures[@]}"; do
-      arch=${arch//./-}
-      arch_underscore=${arch//-/_}
-      exists=$(echo "$latest_release" | grep browser_download_url | grep -E "(${arch}|${arch_underscore})\"" | cut -d '"' -f 4)
-      if [ -n "$exists" ]; then
-        echo "Downloading $binary for architecture $arch"
-        curl -sL -o src/KSail/assets/binaries/"${binary}"_"${arch}""$([ "$is_tarball" = true ] && echo ".tar.gz")" "$exists"
-        if [ "$is_tarball" = true ]; then
-          echo "Extracting tarball"
-          tar -xzf src/KSail/assets/binaries/"${binary}"_"${arch}".tar.gz -C src/KSail/assets/binaries/
-          rm src/KSail/assets/binaries/"${binary}"_"${arch}".tar.gz
-        fi
-        if [ -n "$subfolder" ]; then
-          echo "Moving binary from subfolder $subfolder"
-          mv -f src/KSail/assets/binaries/"${subfolder}"/"$binary" src/KSail/assets/binaries/"${binary}"_"${arch}"
-          rm -rf src/KSail/assets/binaries/"${subfolder}"
-        elif [ -e src/KSail/assets/binaries/"$binary" ]; then
-          mv -f src/KSail/assets/binaries/"$binary" src/KSail/assets/binaries/"${binary}"_"${arch}"
-        fi
-        chmod +x src/KSail/assets/binaries/"${binary}"_"${arch}"
+  for arch in "${architectures[@]}"; do
+    exists=$(echo "$latest_release" | grep browser_download_url | grep "$arch" | cut -d '"' -f 4)
+    if [ -n "$exists" ]; then
+      echo "Downloading $binary $version_latest for architecture $arch"
+      curl -s -L "$exists" -o src/KSail/assets/binaries/"${binary}"_"${arch}""$([ "$is_tarball" = true ] && echo ".tar.gz")"
+      if [ "$is_tarball" = true ]; then
+        echo "Extracting tarball"
+        tar -xzf src/KSail/assets/binaries/"${binary}"_"${arch}".tar.gz -C src/KSail/assets/binaries/
+        rm src/KSail/assets/binaries/"${binary}"_"${arch}".tar.gz
       fi
-    done
-    echo "Updating version in requirements.txt"
-    if [ "$version_current" = "v0.0.0" ]; then
-      echo "${binary}_${version_latest}" >>src/KSail/assets/binaries/requirements.txt
-    else
-      sed -i'' -e "s/^${binary}_.*/${binary}_${version_latest}/" src/KSail/assets/binaries/requirements.txt
+      if [ -n "$subfolder" ]; then
+        echo "Moving binary from subfolder $subfolder"
+        mv -f src/KSail/assets/binaries/"${subfolder}"/"$binary" src/KSail/assets/binaries/"${binary}"_"${arch}"
+        rm -rf src/KSail/assets/binaries/"${subfolder}"
+      elif [ -e src/KSail/assets/binaries/"$binary" ]; then
+        mv -f src/KSail/assets/binaries/"$binary" src/KSail/assets/binaries/"${binary}"_"${arch}"
+      fi
+      chmod +x src/KSail/assets/binaries/"${binary}"_"${arch}"
     fi
-  else
-    echo "$binary is already up to date"
-    echo ""
-  fi
+  done
+  find src/KSail/assets/binaries -name "LICENSE" -type f -delete
 }
 
 set -e
@@ -60,5 +39,3 @@ download_and_update "yannh/kubeconform" "kubeconform" true
 download_and_update "kubernetes-sigs/kustomize" "kustomize" true
 download_and_update "FiloSottile/age" "age-keygen" true "age"
 download_and_update "getsops/sops" "sops" false
-echo "Removing LICENSE files"
-find src/KSail/assets/binaries -name "LICENSE" -type f -delete
