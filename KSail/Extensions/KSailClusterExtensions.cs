d9929b42abc4c325a13bff1874704e8e1dc81a85
Nikolai Emil Damm <nikolaiemildamm@icloud.com>
2024-11-04 14:56:25 +0100

    fix: rename KSailClusterExtensions and enhance property value handling with default initialization

--- KSail/Extensions/KSailClusterExtensions.cs
+++ KSail/Extensions/KSailClusterExtensions.cs
@@ -1,6 +1,6 @@
 using KSail.Models;
 
-namespace KSail;
+namespace KSail.Extensions;
 
 /// <summary>
 /// Extension methods for the <see cref="KSailCluster"/> class.
@@ -22,31 +22,37 @@ public static class KSailClusterExtensions
 
     string[] properties = propertyPath.Split('.');
     object? currentObject = config;
+    object? defaultObject = new KSailCluster();
 
     for (int i = 0; i < properties.Length; i++)
     {
       string propertyName = properties[i];
       var property = (currentObject?.GetType().GetProperty(propertyName)) ?? throw new ArgumentException($"Property '{propertyName}' not found on type '{currentObject?.GetType().FullName}'.");
+      var defaultProperty = (defaultObject?.GetType().GetProperty(propertyName)) ?? throw new ArgumentException($"Property '{propertyName}' not found on type '{defaultObject?.GetType().FullName}'.");
       if (i == properties.Length - 1)
       {
         // If it's the last property in the path, set the value
         object? currentValue = property.GetValue(currentObject);
-        if (!Equals(currentValue, value))
-        {
+        object? defaultValue = defaultProperty.GetValue(defaultObject);
+        // if currentValue is not value and value is different from the value it is set to at initialization
+        if (!Equals(currentValue, value) && !Equals(value, defaultValue))
           property.SetValue(currentObject, value);
-        }
       }
       else
       {
         // Traverse to the next object in the path
         object? nextObject = property.GetValue(currentObject);
+        object? nextDefaultObject = defaultProperty.GetValue(defaultObject);
         if (nextObject == null)
         {
           // Initialize the property if it's null
           nextObject = Activator.CreateInstance(property.PropertyType);
+          nextDefaultObject = Activator.CreateInstance(defaultProperty.PropertyType);
           property.SetValue(currentObject, nextObject);
+          property.SetValue(defaultObject, nextDefaultObject);
         }
         currentObject = nextObject;
+        defaultObject = nextDefaultObject;
       }
     }
   }
