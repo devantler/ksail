ff536e466000061fd5e7559dca500219c40594ce
Nikolai Emil Damm <neq@energinet.dk>
2024-12-23 12:18:06 +0100

    Add JSON schema generation
    Signed-off-by: Nikolai Emil Damm <neq@energinet.dk>

---
+++ tests/KSail.Models.Tests/JSONSchemaGeneration.cs
@@ -0,0 +1,27 @@
+using System.Text.Json;
+using System.Text.Json.Schema;
+
+namespace KSail.Models.Tests;
+
+/// <summary>
+/// Tests that a JSONSchema is generate from the <see cref="KSailCluster"/> object
+/// </summary>
+public class KSailClusterJSONSchemaGeneration
+{
+  /// <summary>
+  /// Tests that a JSONSchema is generated from the <see cref="KSailCluster"/> object.
+  /// </summary>
+  /// <returns></returns>
+  [Fact]
+  public async Task GenerateJSONSchemaFromKSailCluster_ShouldReturnJSONSchema()
+  {
+    // Arrange
+    var options = JsonSerializerOptions.Default;
+    var schema = options.GetJsonSchemaAsNode(typeof(KSailCluster));
+
+    // Act
+    // Assert
+    _ = await Verify(schema.ToString());
+    await File.WriteAllTextAsync("../../../../../../ksail-cluster-schema.json", schema.ToString());
+  }
+}
