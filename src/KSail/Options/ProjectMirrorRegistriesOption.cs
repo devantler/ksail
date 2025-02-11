ebe906569dfb1dd2c4165ade8ae36e9a23e36e09
Nikolai Emil Damm <neq@energinet.dk>
2025-02-11 23:09:49 +0100

    Change ProjectMirrorRegistriesOption to accept nullable boolean
    Signed-off-by: Nikolai Emil Damm <neq@energinet.dk>

--- src/KSail/Options/ProjectMirrorRegistriesOption.cs
+++ src/KSail/Options/ProjectMirrorRegistriesOption.cs
@@ -2,7 +2,7 @@ using System.CommandLine;
 
 namespace KSail.Options;
 
-class ProjectMirrorRegistriesOption() : Option<bool>
+class ProjectMirrorRegistriesOption() : Option<bool?>
 (
   ["-mr", "--mirror-registries"],
   "Enable mirror registries."
