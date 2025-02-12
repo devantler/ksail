ca2d91c2a05980b83fb6d63ed2eecf0e3c3e1c03
Nikolai Emil Damm <neq@energinet.dk>
2025-02-12 21:24:16 +0100

    Refactor `ksail secrets list` command to consolidate option handling and rename project keys option
    Signed-off-by: Nikolai Emil Damm <neq@energinet.dk>

--- src/KSail/Commands/Secrets/Commands/KSailSecretsListCommand.cs
+++ src/KSail/Commands/Secrets/Commands/KSailSecretsListCommand.cs
@@ -2,6 +2,7 @@ using System.CommandLine;
 using Devantler.SecretManager.SOPS.LocalAge;
 using KSail.Commands.Secrets.Handlers;
 using KSail.Commands.Secrets.Options;
+using KSail.Options;
 using KSail.Utils;
 using YamlDotNet.Core;
 
@@ -10,18 +11,19 @@ namespace KSail.Commands.Secrets.Commands;
 sealed class KSailSecretsListCommand : Command
 {
   readonly ExceptionHandler _exceptionHandler = new();
+  readonly ProjectSecretManagerOption _projectSecretManagerOption = new() { Arity = ArgumentArity.ZeroOrOne };
   readonly ShowPrivateKeysOption _showPrivateKeysOption = new() { Arity = ArgumentArity.ZeroOrOne };
   readonly ShowProjectKeysOption _showProjectKeysOption = new() { Arity = ArgumentArity.ZeroOrOne };
   internal KSailSecretsListCommand() : base("list", "List keys")
   {
-    AddOption(_showPrivateKeysOption);
-    AddOption(_showProjectKeysOption);
+    AddOptions();
 
     this.SetHandler(async (context) =>
     {
       try
       {
         var config = await KSailClusterConfigLoader.LoadAsync().ConfigureAwait(false);
+        config.UpdateConfig("Spec.Project.SecretManager", context.ParseResult.GetValueForOption(_projectSecretManagerOption));
         config.UpdateConfig("Spec.CLI.Secrets.List.ShowPrivateKeys", context.ParseResult.GetValueForOption(_showPrivateKeysOption));
         config.UpdateConfig("Spec.CLI.Secrets.List.ShowProjectKeys", context.ParseResult.GetValueForOption(_showProjectKeysOption));
 
@@ -58,5 +60,12 @@ sealed class KSailSecretsListCommand : Command
         context.ExitCode = 1;
       }
     });
+  }
+
+  void AddOptions()
+  {
+    AddOption(_projectSecretManagerOption);
+    AddOption(_showPrivateKeysOption);
+    AddOption(_showProjectKeysOption);
   }
 }
--- src/KSail/Commands/Secrets/Options/ShowProjectKeysOption.cs
+++ src/KSail/Commands/Secrets/Options/ShowProjectKeysOption.cs
@@ -3,6 +3,6 @@ using System.CommandLine;
 namespace KSail.Commands.Secrets.Options;
 
 sealed class ShowProjectKeysOption() : Option<bool?>(
-  ["--show-project-keys", "-spk"],
+  ["--show-project-keys-only", "-spko"],
   "Only show keys used in the current project"
 );
