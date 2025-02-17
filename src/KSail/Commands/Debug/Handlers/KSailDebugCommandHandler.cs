0282b119fde7157a17283a306559cf6a9bab0208
Nikolai Emil Damm <neq@energinet.dk>
2025-02-17 20:38:59 +0100

    chore: add TODO for updating k9s call with pseudo-terminal support
    Signed-off-by: Nikolai Emil Damm <neq@energinet.dk>

--- src/KSail/Commands/Debug/Handlers/KSailDebugCommandHandler.cs
+++ src/KSail/Commands/Debug/Handlers/KSailDebugCommandHandler.cs
@@ -13,6 +13,7 @@ class KSailDebugCommandHandler
   internal async Task<bool> HandleAsync(CancellationToken cancellationToken = default)
   {
     string[] args = [];
+    // TODO: Update k9s call when pseudo-terminal support is added to CLIWrap. See https://github.com/Tyrrrz/CliWrap/issues/225.
     Environment.SetEnvironmentVariable("EDITOR", _config.Spec.Project.Editor.ToString().ToLower(CultureInfo.CurrentCulture));
     var (exitCode, _) = await K9s.RunAsync(args, cancellationToken: cancellationToken).ConfigureAwait(false);
     Environment.SetEnvironmentVariable("EDITOR", null);
