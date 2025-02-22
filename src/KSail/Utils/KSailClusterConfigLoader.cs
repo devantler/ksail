eb0d95225e2e5a526298f6071a541d5864d4274f
Nikolai Emil Damm <neq@energinet.dk>
2025-02-22 16:49:56 +0100

    fix: implement LoadWithGlobalOptions method to utilize global options in KSailClusterConfigLoader
    Signed-off-by: Nikolai Emil Damm <neq@energinet.dk>

--- src/KSail/Utils/KSailClusterConfigLoader.cs
+++ src/KSail/Utils/KSailClusterConfigLoader.cs
@@ -1,7 +1,9 @@
+using System.CommandLine.Invocation;
 using Devantler.KubernetesGenerator.Core.Converters;
 using Devantler.KubernetesGenerator.Core.Inspectors;
 using KSail.Models;
 using KSail.Models.Project;
+using KSail.Options;
 using YamlDotNet.Serialization;
 using YamlDotNet.Serialization.NamingConventions;
 using YamlDotNet.System.Text.Json;
@@ -15,6 +17,30 @@ static class KSailClusterConfigLoader
       .WithTypeConverter(new IntstrIntOrStringTypeConverter())
       .WithTypeConverter(new ResourceQuantityTypeConverter())
       .WithNamingConvention(CamelCaseNamingConvention.Instance).Build();
+
+  internal static async Task<KSailCluster> LoadWithGlobalOptions(GlobalOptions globalOptions, InvocationContext context)
+  {
+    var config = await LoadAsync(
+      context.ParseResult.GetValueForOption(globalOptions.ProjectConfigOption),
+      context.ParseResult.GetValueForOption(globalOptions.ProjectWorkingDirectoryOption),
+      context.ParseResult.GetValueForOption(globalOptions.MetadataNameOption),
+      context.ParseResult.GetValueForOption(globalOptions.ProjectDistributionOption)
+    ).ConfigureAwait(false);
+    config.UpdateConfig("Metadata.Name", context.ParseResult.GetValueForOption(globalOptions.MetadataNameOption));
+    config.UpdateConfig("Spec.Connection.Kubeconfig", context.ParseResult.GetValueForOption(globalOptions.ConnectionKubeconfigOption));
+    config.UpdateConfig("Spec.Connection.Context", context.ParseResult.GetValueForOption(globalOptions.ConnectionContextOption));
+    config.UpdateConfig("Spec.Connection.Timeout", context.ParseResult.GetValueForOption(globalOptions.ConnectionTimeoutOption));
+    config.UpdateConfig("Spec.Project.WorkingDirectory", context.ParseResult.GetValueForOption(globalOptions.ProjectWorkingDirectoryOption));
+    config.UpdateConfig("Spec.Project.ConfigPath", context.ParseResult.GetValueForOption(globalOptions.ProjectConfigOption));
+    config.UpdateConfig("Spec.Project.Distribution", context.ParseResult.GetValueForOption(globalOptions.ProjectDistributionOption));
+    config.UpdateConfig("Spec.Project.DistributionConfigPath", context.ParseResult.GetValueForOption(globalOptions.ProjectDistributionConfigOption));
+    config.UpdateConfig("Spec.Project.Engine", context.ParseResult.GetValueForOption(globalOptions.ProjectEngineOption));
+    config.UpdateConfig("Spec.Project.MirrorRegistries", context.ParseResult.GetValueForOption(globalOptions.ProjectMirrorRegistriesOption));
+    config.UpdateConfig("Spec.Project.SecretManager", context.ParseResult.GetValueForOption(globalOptions.ProjectSecretManagerOption));
+    config.UpdateConfig("Spec.Project.Template", context.ParseResult.GetValueForOption(globalOptions.ProjectTemplateOption));
+    config.UpdateConfig("Spec.Project.Editor", context.ParseResult.GetValueForOption(globalOptions.ProjectEditorOption));
+    return config;
+  }
 
   internal static async Task<KSailCluster> LoadAsync(string? configFilePath = "ksail-config.yaml", string? directory = default, string? name = default, KSailKubernetesDistribution distribution = default)
   {
