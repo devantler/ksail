3be2321f6542b71bfba3f0879c28395ab7053daf
Nikolai Emil Damm <neq@energinet.dk>
2025-02-22 16:48:38 +0100

    fix: refactor KSailDebugCommand to use global options and remove unused options
    Signed-off-by: Nikolai Emil Damm <neq@energinet.dk>

--- src/KSail/Commands/Debug/KSailDebugCommand.cs
+++ src/KSail/Commands/Debug/KSailDebugCommand.cs
@@ -1,6 +1,5 @@
 using System.CommandLine;
 using KSail.Commands.Debug.Handlers;
-using KSail.Commands.Debug.Options;
 using KSail.Options;
 using KSail.Utils;
 
@@ -9,23 +8,14 @@ namespace KSail.Commands.Debug;
 sealed class KSailDebugCommand : Command
 {
   readonly ExceptionHandler _exceptionHandler = new();
-  readonly ConnectionKubeconfigOption _kubeconfigOption = new() { Arity = ArgumentArity.ZeroOrOne };
-  readonly ConnectionContextOption _contextOption = new() { Arity = ArgumentArity.ZeroOrOne };
-  readonly EditorOption _editorOption = new() { Arity = ArgumentArity.ZeroOrOne };
 
-  internal KSailDebugCommand() : base("debug", "Debug a cluster (❤️ K9s)")
+  internal KSailDebugCommand(GlobalOptions globalOptions) : base("debug", "Debug a cluster (❤️ K9s)")
   {
-    AddOption(_kubeconfigOption);
-    AddOption(_contextOption);
-    AddOption(_editorOption);
     this.SetHandler(async (context) =>
     {
       try
       {
-        var config = await KSailClusterConfigLoader.LoadAsync().ConfigureAwait(false);
-        config.UpdateConfig("Spec.Connection.Kubeconfig", context.ParseResult.GetValueForOption(_kubeconfigOption));
-        config.UpdateConfig("Spec.Connection.Context", context.ParseResult.GetValueForOption(_contextOption));
-        config.UpdateConfig("Spec.Project.Editor", context.ParseResult.GetValueForOption(_editorOption));
+        var config = await KSailClusterConfigLoader.LoadWithGlobalOptions(globalOptions, context);
         var handler = new KSailDebugCommandHandler(config);
         context.ExitCode = await handler.HandleAsync(context.GetCancellationToken()).ConfigureAwait(false) ? 0 : 1;
         Console.WriteLine();
